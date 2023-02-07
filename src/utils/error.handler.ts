11539361a953434512e18115102e3c2636d49456
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2023-02-07 04:43:54 -0300

    feat(errors)
      * add a parameter for handler error if
        i would like some more description.

--- src/utils/error.handler.ts
+++ src/utils/error.handler.ts
@@ -1,3 +1,6 @@
 import { Response } from "express"
 
-export const handlerHttp = (res:Response, error: string) => res.status(500).send({error})
+export const handlerHttp = (res:Response, error: string, errorRaw?: any) => {
+  console.log(errorRaw)
+  res.status(500).send({error})
+}
